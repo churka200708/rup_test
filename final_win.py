c43fd31ca1947dcc3dc7a21214918a035a6b4a2c
churka200708 <132600889+churka200708@users.noreply.github.com>
2023-05-18 19:30:43 +0300

    Update final_win.py

--- final_win.py
+++ final_win.py
@@ -1,1 +1,54 @@
+from PyQt5.QtCore import Qt, QTimer, QTime, QLocale
+from PyQt5.QtGui import QFont 
+from PyQt5.QtWidgets import (
+       QApplication, QWidget,
+       QHBoxLayout, QVBoxLayout, QGridLayout,
+       QGroupBox, QRadioButton,
+       QPushButton, QLabel, QListWidget, QLineEdit)
+
+
+
+from instr import
+
+
+class finalWin(Qwidget)
+    def __init__(self,exp):
+        super().__init__()
+        self.exp = exp
+        self.initUI()
+        self.set_appear()
+        self.show()
+        
+        def results(self):
+            if self.exp.age < 7
+            self.index = 0
+            return "нет данных для такого возраста"
+        self.index = (4 * (int(self.exp.t1)+int(self.exp.t2)))
+        if self.exp.age == 7 or self.exp.age == 8 :
+            if self.index >=21:
+                return txt_res1
+            elif self.index <21 and self.index >= 17:
+                 return txt_res2
+            elif self.index <17 and self.index >=12
+                return txt_res3
+            elif self.index <12 and self.index >=6.5
+                return txt_res4
+            else:
+                return txt_res5
+            if self.exp.age == 9 and self.exp.age == 10:  
+                if self.index >=19.5:
+                    return txt_res1
+                elif self.index < 19.5 and self.index >= 15.5
+                    return txt_res2
+                                         
+                          
+
+
+
+
+
+        def initUI(self)
+    pass
+
+
 
